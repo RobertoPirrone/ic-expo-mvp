3f2d2da838e13d18a3487498eaa5c21d8099632b
Roberto Pirrone <roberto.pirrone@most.it>
2025-03-26 12:28:11 +0100

    splashscreen

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 import "react-native-get-random-values";
 import "react-native-polyfill-globals/auto";
+import * as SplashScreen from 'expo-splash-screen';
 globalThis.TextEncoder = TextEncoder;
 window.TextEncoder = TextEncoder;
 import React, { useEffect, useState } from "react";
@@ -8,6 +9,13 @@ import { TextEncoder } from "text-encoding";
 import LoggedOut from "./components/LoggedOut";
 import LoggedIn from "./components/LoggedIn";
 import { useAuth } from "./hooks/useAuth";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({ duration: 1000, fade: true, });
+
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -25,6 +33,7 @@ function App() {
   }, [identity]);
 
   if (!isReady) return null;
+  SplashScreen.hide();
 
   return (
     <View style={styles.container} accessible={true}>
